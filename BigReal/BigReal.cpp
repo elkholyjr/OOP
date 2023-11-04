9ec6195af504a3ad41c6174a2c0494cc11381771
elkholyjr <119688248+elkholyjr@users.noreply.github.com>
2023-11-04 22:02:51 +0200

    Update BigReal.cpp

--- BigReal/BigReal.cpp
+++ BigReal/BigReal.cpp
@@ -1,1 +1,10 @@
-
+// File: BigReal.cpp
+// Purpose: making basic operations but on very big numbers
+// Author: ahmed elkholy , youssef nabeh , anas elessily
+// Section: S1
+// ID: 20220037 , 20220410 , 20220071
+// TA: Nardeen mokhless
+// Date: 4 Nov 2023
+#include "BigReal.h"
+#include <bits/stdc++.h>
+using namespace std;
