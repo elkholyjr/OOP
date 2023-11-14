cd2f5c6e864cdcf7766c0f79833b958fafb15baf
elkholyjr <119688248+elkholyjr@users.noreply.github.com>
2023-11-14 19:11:00 +0200

    Update simulator.cpp

--- Task 3/simulator.cpp
+++ Task 3/simulator.cpp
@@ -21,9 +21,7 @@ CPU::CPU() : registers(16, 0), program_counter(0), is_halted(false) {}
 void CPU::load_register(int register_num, int value) {
     registers[register_num] = value;
 }
-void CPU::add(int destination, int source1, int source2) {
-    registers[destination] = (registers[source1] + registers[source2]) & 0xFF;
-}
+
 void CPU::store(int register_num, int address,Memory& memory) {
     if (address == 0x00) {
         // Write to screen
@@ -35,6 +33,87 @@ void CPU::store(int register_num, int address,Memory& memory) {
 
 void CPU::move(int source, int destination) {
     registers[destination] = registers[source];
+}
+
+void CPU::add(int destination, int source1, int source2) {
+    registers[destination] = (registers[source1] + registers[source2]) & 0xFF;
+}
+
+void CPU::jump(int register_num, int address) {
+    if (registers[register_num] == registers[0]) {
+        program_counter = address / 2 - 1;
+    }
+}
+
+void CPU::halt() {
+    is_halted = true;
+}
+
+// Implementation for Simulator class
+Simulator::Simulator() : memory(255), cpu() {}
+
+void Simulator::read_instructions_from_file(const std::string& filename) {
+    ifstream file(filename);
+    if (!file) {
+        cerr << "Unable to open file: " << filename << endl;
+        return;
+    }
+
+    string line;
+    int address = 0; // Use this variable as the memory address
+
+    while (std::getline(file, line)) {
+        istringstream iss(line);
+        int opcode, operand1, operand2;
+
+        if (!(iss >> std::hex >> opcode >> std::hex >> operand1 >> std::hex >> operand2)) {
+            cerr << "Error processing instruction at address " << address << std::endl;
+            continue;
+        }
+
+        // Store instructions in memory at the specified address
+        memory.load_memory(address++, (opcode << 16) | (operand1 << 8) | operand2);
+    }
+
+    file.close();
+}
+
+
+void Simulator::execute_program() {
+    for (cpu.program_counter = 0; cpu.program_counter < memory.memory.size() && !cpu.is_halted; ++cpu.program_counter) {
+        int instruction = memory.memory[cpu.program_counter];
+        int opcode = (instruction >> 16) & 0xFF;
+        int operand1 = (instruction >> 8) & 0xFF;
+        int operand2 = instruction & 0xFF;
+
+        // Process instruction (opcode, operand1, operand2)
+        switch (opcode) {
+            case 0x1:
+                // Handle opcode 0x1
+                cpu.load_register(operand1, memory.memory[operand2]);
+                break;
+            case 0x2:
+                cpu.load_register(operand1, operand2);
+                break;
+            case 0x3:
+                cpu.store(operand1, operand2, memory);
+                break;
+            case 0x4:
+                cpu.move(operand1, operand2);
+                break;
+            case 0x5:
+                cpu.add(operand1, operand2 & 0x0F, (operand2 & 0xF0) >> 4);
+                break;
+            case 0xB:
+                cpu.jump(operand1, operand2);
+                break;
+            case 0xC:
+                cpu.halt();
+                break;
+            default:
+                cerr << "Unknown opcode: " << opcode << " at instruction " << cpu.program_counter << endl;
+        }
+    }
 }
 
 int Simulator::single_step() {
